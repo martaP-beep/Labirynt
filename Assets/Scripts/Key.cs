006245b77446018f1b44d9a2209856fd87f6eed0
martaP-beep <marta.ios.developer@gmail.com>
2024-11-04 19:58:43 +0100

    lekcja9

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -11,6 +11,17 @@ public enum KeyColor
 public class Key : Pickup
 {
     public KeyColor color;
+
+    public float amplitude = 0.5f; 
+    public float speed = 1.0f;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        // Zapisz pocz¹tkow¹ pozycjê obiektu
+        startPosition = transform.position;
+    }
+
     public override void Picked()
     {
         base.Picked();
@@ -21,6 +32,7 @@ public class Key : Pickup
     // Update is called once per frame
     void Update()
     {
-        
+        float newY = startPosition.y + Mathf.Sin(Time.time * speed) * amplitude;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
     }
 }
